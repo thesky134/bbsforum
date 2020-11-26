e1b644ab8f87f20c95fc7f4c48ac1d6ad4060396
thesky <1767206074@qq.com>
2020-11-26 21:54:25 +0800

    hsc create bbsforum-api

---
+++ bbsforum-api/src/main/java/top/thesk341/bbsforum/BbsforumApplication.java
@@ -0,0 +1,13 @@
+package top.thesk341.bbsforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsforumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BbsforumApplication.class, args);
+    }
+
+}
---
+++ bbsforum-api/src/test/java/top/thesk341/bbsforum/BbsforumApplicationTests.java
@@ -0,0 +1,13 @@
+package top.thesk341.bbsforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsforumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
