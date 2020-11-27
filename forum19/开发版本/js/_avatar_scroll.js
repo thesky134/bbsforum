4bf22432f04ee2a0bd2cc8395098366b917f179b
for-sea <2595140734@qq.com>
2020-11-27 15:10:07 +0800

    xsh 前端页面初步汉化

--- forum19/开发版本/js/_avatar_scroll.js
+++
@@ -1,9 +0,0 @@
-/*
-    Init Avatar List(*custom scroll)
-*/
-(function(){
-    var ttSearchWrapper = $('#tt-pageContent .js-init-scroll');
-    if (ttSearchWrapper){
-       ttSearchWrapper.perfectScrollbar();
-    };
-})();
--- forum19/开发版本/js/_search_compose.js
+++
@@ -1,9 +0,0 @@
-/*
-    *messages-compose.html
-*/
-(function(){
-    var ttComposeResults = $('#tt-pageContent .tt-search-compose .tt-search-results');
-    if (ttComposeResults){
-       ttComposeResults.perfectScrollbar();
-    };
-})();
